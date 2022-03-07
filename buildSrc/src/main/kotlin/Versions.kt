0693bf4de6010ccd043307461a1ffc59e651b932
Mathieu Scotet <mscotet.lmit@gmail.com>
2022-03-07 17:43:41 +0100

    dimens de height par défaut
    sk_tabbar_colortab_root_default_height

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
     const val group = "tech.skot.libraries.sk-tabbar"
-    const val library = "0.1.1"
-    const val framework = "1.1.8"
+    const val library = "0.1.2"
+    const val framework = "1.1.22"
 }
